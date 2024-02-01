4693108aae421685a467831d65e0078a9b911271
Constantin Piber <cp.piber@gmail.com>
2024-02-01 20:46:56 +0100

    Format

--- main.js
+++ main.js
@@ -15,10 +15,10 @@
 // @grant        GM_getValue
 // ==/UserScript==
 
-(async function() {
-  'use strict';
+(async function () {
+  "use strict";
 
-  const form = document.querySelector('.recording > form#form_new');
+  const form = document.querySelector(".recording > form#form_new");
   if (!form) return;
   GM_log("Got the form");
   GM_addStyle(`
@@ -157,13 +157,17 @@
 }
   `);
 
-
   /** General helpers */
 
+  /**
+   * @param {string} tag
+   * @param {Record<string, any> & { children?: Element[], textContent?: string, text?: string }} options
+   * @returns {HTMLElement}
+   */
   function create(tag, options = {}) {
     const el = document.createElement(tag);
     for (const key in options) {
-      if (key === 'textContent' || key === 'text') {
+      if (key === "textContent" || key === "text") {
         el.textContent = options[key];
       } else if (key === "children") {
         el.append.apply(el, options[key]);
@@ -174,24 +178,71 @@
     return el;
   }
 
+  /**
+   * @param {HTMLElement} el
+   * @param {string} content
+   */
   function setContent(el, content) {
     if (el.textContent !== content) el.textContent = content;
   }
 
+  /**
+   * @param {HTMLElement} el
+   * @param {Date} date
+   */
   function setTime(el, date) {
     if (el.dateTime !== date.toISOString()) el.dateTime = date.toISOString();
     setContent(el, date.toLocaleTimeString());
+    el.style.display = "";
   }
 
+  /**
+   * @overload
+   * @param {Date} startOrDuration
+   * @param {Date} end
+   * @returns {string}
+   */
+  /**
+   * @overload
+   * @param {number} startOrDuration
+   * @returns {string}
+   */
+  /**
+   * @param {Date|number} startOrDuration
+   * @param {Date?} end
+   * @returns {string}
+   */
   function getDuration(startOrDuration, end = undefined) {
-    const d = end !== undefined ? end.getTime() - startOrDuration.getTime() : startOrDuration;
-    const duration = d / 1000 / 60/ 60;
+    const d =
+      end !== undefined
+        ? end.getTime() - startOrDuration.getTime()
+        : startOrDuration;
+    const duration = d / 1000 / 60 / 60;
     return duration.toFixed(2);
   }
 
+  /**
+   * @overload
+   * @param {Date} startOrDuration
+   * @param {Date} end
+   * @returns {string}
+   */
+  /**
+   * @overload
+   * @param {number} startOrDuration
+   * @returns {string}
+   */
+  /**
+   * @param {Date|number} startOrDuration
+   * @param {Date?} end
+   * @returns {string}
+   */
   function formatDuration(startOrDuration, end = undefined) {
-    let duration = end !== undefined ? end.getTime() - startOrDuration.getTime() : startOrDuration;
-    let retstr = '';
+    let duration =
+      end !== undefined
+        ? end.getTime() - startOrDuration.getTime()
+        : startOrDuration;
+    let retstr = "";
     if (duration > 60 * 60 * 1000) {
       const h = Math.floor(duration / (60 * 60 * 1000));
       retstr += `${h}h `;
@@ -207,19 +258,27 @@
     return retstr;
   }
 
+  /**
+   * @param {number} number
+   * @param {number} digits
+   * @returns {string}
+   */
   function formatNumber(number, digits = 2) {
     const sig = Math.round(number);
     let ret = sig.toString();
-    while (ret.length < digits) ret = '0' + ret;
+    while (ret.length < digits) ret = "0" + ret;
     if (number - sig === 0) return ret;
-    return ret + '.' + (number - sig).toString();
+    return ret + "." + (number - sig).toString();
   }
 
-
   /** Data */
-  const tasks = (await GM.getValue('tasks', [])).map(t => {
+
+  /**
+   * @type {Array<{ start: Date, end?: Date }>}
+   */
+  const tasks = (await GM.getValue("tasks", [])).map((t) => {
     t.start = new Date(t.start);
-    if ('end' in t) t.end = new Date(t.end);
+    if ("end" in t) t.end = new Date(t.end);
     return t;
   });
   const total = {
@@ -227,62 +286,126 @@
     end: new Date(),
     duration: 0,
   };
-
+  /**
+   * @type {{num:number,type:'start'|'end'}|undefined}
+   */
+  let currently_editing = undefined;
 
   /** UI */
 
-  const wrap = create('div', { class: 'ext-area' });
+  const wrap = create("div", { class: "ext-area" });
   form.parentElement.before(wrap);
 
-  const summary = create('div', { class: 'ext-summary', children: [
-    create('span', { class: 'text-bold', text: 'Total:' }),
-    create('span', { text: 'Start:' }),
-    create('time', { name: 'start' }),
-    create('span', { text: 'End:', class: 'end' }),
-    create('time', { name: 'end', class: 'end' }),
-    create('span', { text: 'Duration:' }),
-    create('span', { name: 'duration' }),
-  ] });
-  const buttonrow = create('div', { class: 'ext-main-row', children: [
-    create('button', { text: '▶️ Start', class: 'ext-button-main', name: 'start-button' }),
-    summary,
-    create('div', { name: 'apply-wrapper', children: [
-      create('a', { name: 'apply-hours', href: '#apply-hours', text: 'apply hours', class: 'ext-link' }),
-      create('a', { name: 'apply-time', href: '#apply-time', text: 'apply times', class: 'ext-link' }),
-      create('a', { name: 'apply-both', href: '#apply', text: 'apply both', class: 'ext-link' }),
-      create('a', { name: 'clear', href: '#clear', text: 'clear', class: 'ext-link-danger' }),
-    ] }),
-    create('a', { name: 'clear', href: '#clear', text: 'clear', class: 'ext-link-danger' }),
-  ] });
+  const summary = create("div", {
+    class: "ext-summary",
+    children: [
+      create("span", { class: "text-bold", text: "Total:" }),
+      create("span", { text: "Start:" }),
+      create("time", { name: "start" }),
+      create("span", { text: "End:", class: "end" }),
+      create("time", { name: "end", class: "end" }),
+      create("span", { text: "Duration:" }),
+      create("span", { name: "duration" }),
+    ],
+  });
+  const buttonrow = create("div", {
+    class: "ext-main-row",
+    children: [
+      create("button", {
+        text: "▶️ Start",
+        class: "ext-button-main",
+        name: "start-button",
+      }),
+      summary,
+      create("div", {
+        name: "apply-wrapper",
+        children: [
+          create("a", {
+            name: "apply-hours",
+            href: "#apply-hours",
+            text: "apply hours",
+            class: "ext-link",
+          }),
+          create("a", {
+            name: "apply-time",
+            href: "#apply-time",
+            text: "apply times",
+            class: "ext-link",
+          }),
+          create("a", {
+            name: "apply-both",
+            href: "#apply",
+            text: "apply both",
+            class: "ext-link",
+          }),
+          create("a", {
+            name: "clear",
+            href: "#clear",
+            text: "clear",
+            class: "ext-link-danger",
+          }),
+        ],
+      }),
+      create("a", {
+        name: "clear",
+        href: "#clear",
+        text: "clear",
+        class: "ext-link-danger",
+      }),
+    ],
+  });
   wrap.append(buttonrow);
 
-  const tasksrow = create('div', { class: 'ext-tasks-row' });
+  const tasksrow = create("div", { class: "ext-tasks-row" });
   buttonrow.before(tasksrow);
 
   function renderTasks() {
     while (tasksrow.childElementCount < tasks.length) {
-      tasksrow.append(create('div', { class: 'ext-task', children: [
-        create('span', { text: 'Start:' }),
-        create('time', { name: 'start' }),
-        create('span', { text: 'End:', class: 'end' }),
-        create('time', { name: 'end', class: 'end' }),
-        create('span', { text: 'Duration:' }),
-        create('span', { name: 'duration' }),
-        create('div', { class: 'running', name: 'stop-wrapper', children: [
-          create('button', { name: 'stop-button', text: '⏹️ Stop', class: 'ext-button-secondary' }),
-        ] }),
-        create('a', { name: 'remove', href: '#remove', title: 'remove', text: '❌', class: 'ext-link-danger' }),
-      ] }));
+      tasksrow.append(
+        create("div", {
+          class: "ext-task",
+          children: [
+            create("span", { text: "Start:" }),
+            create("time", { name: "start" }),
+            create("span", { text: "End:", class: "end" }),
+            create("time", { name: "end", class: "end" }),
+            create("span", { text: "Duration:" }),
+            create("span", { name: "duration" }),
+            create("div", {
+              class: "running",
+              name: "stop-wrapper",
+              children: [
+                create("button", {
+                  name: "stop-button",
+                  text: "⏹️ Stop",
+                  class: "ext-button-secondary",
+                }),
+              ],
+            }),
+            create("a", {
+              name: "remove",
+              href: "#remove",
+              title: "remove",
+              text: "❌",
+              class: "ext-link-danger",
+            }),
+          ],
+        })
+      );
     }
-    while (tasksrow.childElementCount > tasks.length) tasksrow.lastElementChild.remove();
+    while (tasksrow.childElementCount > tasks.length)
+      tasksrow.lastElementChild.remove();
     GM_log("render", tasks.length, "tasks");
     for (let i = 0; i < tasks.length; ++i) {
       const task = tasks[i];
       const taskel = tasksrow.children[i];
       setTime(taskel.querySelector('[name="start"]'), task.start);
       if (task.end) setTime(taskel.querySelector('[name="end"]'), task.end);
-      setContent(taskel.querySelector('[name="duration"]'), formatDuration(task.start, task.end ? task.end : new Date()));
-      taskel.classList.toggle('has-ended', !!task.end);
+      setContent(
+        taskel.querySelector('[name="duration"]'),
+        formatDuration(task.start, task.end ? task.end : new Date())
+      );
+      taskel.classList.toggle("has-ended", !!task.end);
       taskel.dataset.task = i;
     }
   }
@@ -291,13 +414,22 @@
     if (total.start === undefined || total.end === undefined) return;
     setTime(summary.querySelector('[name="start"]'), total.start);
     setTime(summary.querySelector('[name="end"]'), total.end);
-    setContent(summary.querySelector('[name="duration"]'), formatDuration(total.duration));
+    setContent(
+      summary.querySelector('[name="duration"]'),
+      formatDuration(total.duration)
+    );
   }
 
   function renderMain() {
-    wrap.classList.toggle('has-running-tasks', tasks.some(e => !('end' in e)));
-    wrap.classList.toggle('has-finished-tasks', tasks.some(e => ('end' in e)));
-    wrap.classList.toggle('has-tasks', tasks.length > 0);
+    wrap.classList.toggle(
+      "has-running-tasks",
+      tasks.some((e) => !("end" in e))
+    );
+    wrap.classList.toggle(
+      "has-finished-tasks",
+      tasks.some((e) => "end" in e)
+    );
+    wrap.classList.toggle("has-tasks", tasks.length > 0);
 
     calculateTotal();
     renderTasks();
@@ -306,9 +438,11 @@
 
   // dynamic render
   {
+    /** @type {number} */
     let previousTS = undefined; // ms
+    /** @param {number} ts */
     function render(ts) {
-      if (previousTS === undefined || (ts - previousTS) > 500) {
+      if (previousTS === undefined || ts - previousTS > 500) {
         renderMain();
         previousTS = ts;
       }
@@ -316,7 +450,6 @@
     }
     unsafeWindow.requestAnimationFrame(render);
   }
-
 
   /** Logic */
 
@@ -327,59 +460,103 @@
     total.duration = 0;
     for (let i = 0; i < tasks.length; ++i) {
       if (!tasks[i].end) continue;
-      if (total.start === undefined || tasks[i].start.getTime() < total.start.getTime()) total.start = tasks[i].start;
-      if (total.end === undefined || tasks[i].end.getTime() < total.end.getTime()) total.end = tasks[i].end;
+      if (
+        total.start === undefined ||
+        tasks[i].start.getTime() < total.start.getTime()
+      )
+        total.start = tasks[i].start;
+      if (
+        total.end === undefined ||
+        tasks[i].end.getTime() < total.end.getTime()
+      )
+        total.end = tasks[i].end;
       total.duration += tasks[i].end.getTime() - tasks[i].start.getTime();
     }
   }
 
+  /**
+   * @param {typeof tasks} data
+   */
   async function save(data = undefined) {
     if (data === undefined) data = tasks;
-    await GM.setValue('tasks', data.map(t => ({
-      ...t,
-      start: t.start.getTime(),
-      end: t.end?.getTime(),
-    })));
+    await GM.setValue(
+      "tasks",
+      data.map((t) => ({
+        ...t,
+        start: t.start.getTime(),
+        end: t.end?.getTime(),
+      }))
+    );
   }
 
-  tasksrow.addEventListener('click', function (ev) {
-    const taskel = ev.target.closest('.ext-task');
+  tasksrow.addEventListener("click", function (ev) {
+    const taskel = /** @type {HTMLElement} */ (ev.target).closest(".ext-task");
     if (!taskel) return;
     ev.preventDefault();
-    if (ev.target.name === 'remove') {
+    const name = ev.target.name ?? ev.target.getAttribute("name");
+    if (name === "remove") {
       tasks.splice(+taskel.dataset.task, 1);
-    } else if (ev.target.name === 'stop-button') {
+    } else if (name === "stop-button") {
       tasks[+taskel.dataset.task].end = new Date();
-    } else { return; }
+    } else if (name === "start" || name === "end") {
+      if (currently_editing !== undefined) return;
+      currently_editing = { num: +taskel.dataset.task, type: name };
+    } else {
+      return;
+    }
     renderMain();
     save();
   });
-  buttonrow.addEventListener('click', function (ev) {
+  buttonrow.addEventListener("click", function (ev) {
     ev.preventDefault();
-    if (ev.target.name === 'start-button') {
+    const name = ev.target.name ?? ev.target.getAttribute("name");
+    if (name === "start-button") {
       tasks.push({ start: new Date() });
       save();
-    } else if (ev.target.name === 'clear') {
+    } else if (name === "clear") {
       tasks.splice(0);
       save();
-    } else if (ev.target.name === 'apply-hours') {
+    } else if (name === "apply-hours") {
       calculateTotal();
-      document.getElementById('recording_hours').value = getDuration(total.duration);
-    } else if (ev.target.name === 'apply-time') {
+      document.getElementById("recording_hours").value = getDuration(
+        total.duration
+      );
+    } else if (name === "apply-time") {
       calculateTotal();
-      document.getElementById('workingtime_start').value = `${total.start.getHours()}:${formatNumber(total.start.getMinutes())}`;
-      document.getElementById('workingtime_end').value = `${total.end.getHours()}:${formatNumber(total.end.getMinutes())}`;
-    } else if (ev.target.name === 'apply-both') {
+      document.getElementById(
+        "workingtime_start"
+      ).value = `${total.start.getHours()}:${formatNumber(
+        total.start.getMinutes()
+      )}`;
+      document.getElementById(
+        "workingtime_end"
+      ).value = `${total.end.getHours()}:${formatNumber(
+        total.end.getMinutes()
+      )}`;
+    } else if (name === "apply-both") {
       calculateTotal();
-      document.getElementById('recording_hours').value = getDuration(total.duration);
-      document.getElementById('workingtime_start').value = `${total.start.getHours()}:${formatNumber(total.start.getMinutes())}`;
-      document.getElementById('workingtime_end').value = `${total.end.getHours()}:${formatNumber(total.end.getMinutes())}`;
-    } else { return; }
-    if (['apply-hours', 'apply-time', 'apply-both'].indexOf(ev.target.name) !== -1) {
+      document.getElementById("recording_hours").value = getDuration(
+        total.duration
+      );
+      document.getElementById(
+        "workingtime_start"
+      ).value = `${total.start.getHours()}:${formatNumber(
+        total.start.getMinutes()
+      )}`;
+      document.getElementById(
+        "workingtime_end"
+      ).value = `${total.end.getHours()}:${formatNumber(
+        total.end.getMinutes()
+      )}`;
+    } else {
+      return;
+    }
+    if (["apply-hours", "apply-time", "apply-both"].indexOf(name) !== -1) {
       // fake-save no tasks for better experience
       // if the user navigates away after applying, they likely don't want to see the tasks again on next load
-      save(tasks.filter(t => !('end' in t)));
+      save(tasks.filter((t) => !("end" in t)));
     }
     renderMain();
   });
 })();
+
